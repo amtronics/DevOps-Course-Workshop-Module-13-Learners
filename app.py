01c4247012f32830b13b50c4b3dbac520905ea48
Amin Gholamzadeh <amin.gholamzadeh@etas.com>
2024-01-24 11:54:17 +0000

    add app insights

--- app.py
+++ app.py
@@ -5,10 +5,24 @@ from werkzeug.utils import redirect
 from flask_config import Config
 from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
 from scheduled_jobs import initialise_scheduled_jobs
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
+from  opencensus.ext.azure.log_exporter import AzureLogHandler
+
 from products import create_product_download
+
 import requests
+
 app = Flask(__name__)
 app.config.from_object(Config)
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(),
+    sampler=ProbabilitySampler(rate=1.0),
+)
+
+app.logger.addHandler(AzureLogHandler())
 
 initialise_database(app)
 initialise_scheduled_jobs(app)
